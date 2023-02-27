f10bdc0215c3de3b41b1f78d3aaa3dd77f4a2f47
mobileapps23 <40164951+mobileapps23@users.noreply.github.com>
2023-02-27 19:46:31 +0300

    swift class added

---
+++ RCTAppleHealthKit/SwiftInterfaceFiles.h
@@ -0,0 +1,11 @@
+//
+//  SwiftInterfaceFiles.h
+//  RCTAppleHealthKit
+//
+//  Created by Anastasia Mishur on 27.02.23.
+//  Copyright © 2023 Greg Wilson. All rights reserved.
+//
+
+@interface TestSwiftClass()
+- (NSString *) returnValue;
+@end
